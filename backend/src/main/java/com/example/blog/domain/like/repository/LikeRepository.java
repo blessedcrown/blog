7b941071a172e9b6a0d221ad224f7b04dcd180f1
blessedcrown <blessedcrown1@gmail.com>
2022-05-07 13:44:11 +0900

    creates repository for each entities

---
+++ backend/src/main/java/com/example/blog/domain/like/repository/LikeRepository.java
@@ -0,0 +1,9 @@
+package com.example.blog.domain.like.repository;
+
+import com.example.blog.domain.like.entity.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+}
