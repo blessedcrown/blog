908c29889c27996b8d33161aec7cd9bc71767b1a
Zerro97 <khuibeom@gmail.com>
2022-05-08 19:57:45 +0900

    Removed picture domain for backend to work (temporarily) and added cors policy for post API

--- backend/src/main/java/com/example/blog/domain/picture/service/PictureService.java
+++
@@ -1,9 +0,0 @@
-package com.example.blog.domain.picture.service;
-
-import com.example.blog.domain.picture.dto.PictureDto;
-
-public interface PictureService {
-    public Long saveFile(PictureDto requestDto);
-
-    public PictureDto getPictureById(Long id);
-}
--- backend/src/main/java/com/example/blog/domain/post/controller/PostController.java
+++ backend/src/main/java/com/example/blog/domain/post/controller/PostController.java
@@ -8,7 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/post")
 @RestController
